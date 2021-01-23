2690e6443d268706e9b62a63553eebe11b2e5d3a
y00ujin <yujngim43@gmail.com>
2021-01-23 16:12:19 +0900

    2021.01.23

--- FaceBook Login/ViewController.swift
+++ FaceBook Login/ViewController.swift
@@ -1,19 +1,55 @@
-//
-//  ViewController.swift
-//  FaceBook Login
-//
-//  Created by 김유진 on 2021/01/23.
-//
 
-import UIKit
+// Swift //
+// Add this to the header of your file, e.g. in ViewController.swift import FBSDKLoginKit
+// Add this to the body
 
-class ViewController: UIViewController {
+import FBSDKLoginKit
 
+class ViewController: UIViewController, LoginButtonDelegate {
+    
+    
+    // Swift
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let token = AccessToken.current, !token.isExpired {
+            let token = token.tokenString
+            
+            let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                     parameters: ["fields": "email, name"],
+                                                     tokenString: token,
+                                                     version: nil,
+                                                     httpMethod: .get)
+            
+            request.start(completionHandler: {connection, result, error in
+                print("\(result)")
+            })
+            
+        }else{
+            let loginButton = FBLoginButton()
+            loginButton.center = view.center
+            loginButton.delegate = self
+            loginButton.permissions = ["public_profile", "email"]
+            view.addSubview(loginButton)
+        }
     }
-
-
+    
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        let token = result?.token?.tokenString
+        
+        let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                 parameters: ["fields": "email, name"],
+                                                 tokenString: token,
+                                                 version: nil,
+                                                 httpMethod: .get)
+        
+        request.start(completionHandler: {connection, result, error in
+            print("\(result)")
+        })
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        
+    }
+    
 }
-
